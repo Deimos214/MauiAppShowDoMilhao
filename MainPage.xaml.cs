39315d231ead367daed2031ca852d566c8619592
Usuario <Usuario@CPD03-M07>
2024-09-23 15:55:00 -0300

    ...

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiAppShowDoMilhao
+﻿using Plugin.Maui.Audio;
+
+namespace MauiAppShowDoMilhao
 {
     public partial class MainPage : ContentPage
     {
@@ -13,7 +15,85 @@
             lbl_nivel.Text = "Fácil";
             lbl_premio.Text = premio.ToString("C");
             lbl_pergunta_numero.Text = pergunta_count.ToString();
+
+            Stream track = FileSystem.OpenAppPackageFileAsync("0.mp3").Result;
+            AudioManager.Current.CreatePlayer(track).Play();
         }
+        private void toca_som()
+        {
+            string track = "";
+
+            switch (pergunta_count)
+            {
+                case 1:
+                    track = "1.wav";
+                    break;
+
+                case 2:
+                    track = "2.wav";
+                    break;
+
+                case 3:
+                    track = "3.wav";
+                    break;
+
+                case 4:
+                    track = "4.wav";
+                    break;
+
+                case 5:
+                    track = "5.wav";
+                    break;
+
+                case 6:
+                    track = "6.wav";
+                    break;
+
+                case 7:
+                    track = "7.wav";
+                    break;
+
+                case 8:
+                    track = "8.wav";
+                    break;
+
+                case 9:
+                    track = "9.wav";
+                    break;
+
+                case 10:
+                    track = "10.wav";
+                    break;
+
+                case 11:
+                    track = "11.wav";
+                    break;
+
+                case 12:
+                    track = "12.wav";
+                    break;
+
+                case 13:
+                    track = "13.wav";
+                    break;
+
+                case 14:
+                    track = "14.wav";
+                    break;
+
+                case 15:
+                    track = "15.wav";
+                    break;
+                case 16:
+                    track = "16.wav";
+                    break;
+            }
+
+            AudioManager.Current.CreatePlayer(
+                FileSystem.OpenAppPackageFileAsync(track).Result).Play();
+        }
+
+
 
         private void Button_Clicked_Proxima(object sender, EventArgs e)
         {
@@ -89,6 +169,7 @@
                 lbl_premio.Text = premio.ToString("C");
                 lbl_pergunta_numero.Text = pergunta_count.ToString();
             }
+
         }
 
 
